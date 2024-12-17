ad5bd9646ae0932886cb17a909e4f8cc6ae032ce
Aryan Kushwah <0902cs231026@rjit.ac.in>
2024-12-18 00:19:41 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -17,23 +17,18 @@ data = response.json()
 
 json_data = json.dumps(data, indent=4)
 
-# print(data['items'][0]['volumeInfo']['title'])
-# for i in range(len(data['items'])):
-#     print(i)
-# print(json_data)
+# print(data)
 list_of_books = []
 image_links = []
 
-for i in range(10):
-    list_of_books.append(data['items'][i]["volumeInfo"]["title"])
-    image_links.append(
-        data.get("items", [{}])[i]
-        .get("volumeInfo", {})
-        .get("imageLinks", {})
-        .get("smallThumbnail")
-    )
+for item in data.get('items', []):
+  volumeInfo = item.get('volumeInfo', {})
+  title = volumeInfo.get('title', '')
+  imageLink = volumeInfo.get('imageLinks', {}).get('thumbnail', '')
+  list_of_books.append(title)
+  image_links.append(imageLink)
 
-print(list_of_books)
+# print(list_of_books)
 df = pd.DataFrame({"title": list_of_books, "link": image_links})
 
 button = st.button("Read")
@@ -42,18 +37,92 @@ if button:
 
     if book in df["title"].values:
 
-        print("book in df")
         idx = df[df["title"] == book].index[0]
         sim_scores = sorted(
             list(enumerate(sim_test[idx])), key=lambda x: x[1], reverse=True
         )
-        print(sorted(list(enumerate(sim_test[idx])), key=lambda x: x[1], reverse=True))
         first_elements = [item[0] for item in sim_scores]
         suggestions = df.iloc[first_elements]['title']
         
-        st.dataframe(suggestions)
+        df['title'] = df['title'][first_elements]
+        print(df['title'])
+        
+ 
+        col1, col2, col3 = st.columns(3)
+        
+        
+        with st.container():
+            
+            with col1:
+                st.image(df['link'][0])
+                st.text(df['title'][0])
+
+            with col2:
+                st.image(df['link'][1])
+                st.text(df['title'][1])
+
+
+            with col3:
+                st.image(df['link'][2])
+                st.text(df['title'][2])
+
+                
+        with st.container():
+            
+            with col1:
+                st.image(df['link'][3])
+                st.text(df['title'][3])
+                
+
+            with col2:
+                st.image(df['link'][4])
+                st.text(df['title'][4])
+
+            with col3:
+                st.image(df['link'][5])
+                st.text(df['title'][5])
+                
+                
+        
 
     else:
         print("Book not found in the dataset.")
-        print(df["title"])
         st.subheader("Book Not Found")
+        st.text("Did You Mean This?")
+        
+        col1, col2, col3 = st.columns(3)
+        
+        
+        with st.container():
+            
+            with col1:
+                st.image(df['link'][0])
+                st.text(df['title'][0])
+
+            with col2:
+                st.image(df['link'][1])
+                st.text(df['title'][1])
+
+            with col3:
+                st.image(df['link'][2])
+                st.text(df['title'][2])
+                
+
+        with st.container():
+            
+            with col1:
+                st.image(df['link'][3])
+                st.text(df['title'][3])
+
+            with col2:
+                st.image(df['link'][4])
+                st.text(df['title'][4])
+
+            with col3:
+                st.image(df['link'][5])
+                st.text(df['title'][5])
+                
+        
+        
+        
+    
